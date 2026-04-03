ae7c29b7552f43fddb09c97badfde2c67c50a768
Yuji OKAMOTO <okamyuji@gmail.com>
2026-04-03 19:40:15 +0900

    refactor: migrate Windows mouse-move logic to Keeper interface

---
+++ keeper_windows.go
@@ -0,0 +1,71 @@
+//go:build windows
+
+package main
+
+import (
+	"fmt"
+	"time"
+	"unsafe"
+
+	"golang.org/x/sys/windows"
+)
+
+var (
+	user32           = windows.NewLazyDLL("user32.dll")
+	procGetCursorPos = user32.NewProc("GetCursorPos")
+	procSetCursorPos = user32.NewProc("SetCursorPos")
+)
+
+type POINT struct {
+	X int32
+	Y int32
+}
+
+func getCursorPos() (x, y int32) {
+	var pt POINT
+	procGetCursorPos.Call(uintptr(unsafe.Pointer(&pt)))
+	return pt.X, pt.Y
+}
+
+func setCursorPos(x, y int32) {
+	procSetCursorPos.Call(uintptr(x), uintptr(y))
+}
+
+type mouseMoveKeeper struct {
+	interval int
+	done     chan struct{}
+}
+
+func (k *mouseMoveKeeper) Name() string { return "mouse-move" }
+
+func (k *mouseMoveKeeper) Start() error {
+	k.done = make(chan struct{})
+	go func() {
+		ticker := time.NewTicker(time.Duration(k.interval) * time.Second)
+		defer ticker.Stop()
+		for {
+			select {
+			case <-k.done:
+				return
+			case <-ticker.C:
+				x, y := getCursorPos()
+				setCursorPos(x+1, y)
+				time.Sleep(100 * time.Millisecond)
+				setCursorPos(x, y)
+				fmt.Printf("マウスを移動: (%d, %d) -> 1px右 -> 元の位置\n", x, y)
+			}
+		}
+	}()
+	return nil
+}
+
+func (k *mouseMoveKeeper) Stop() error {
+	if k.done != nil {
+		close(k.done)
+	}
+	return nil
+}
+
+func platformKeepers(interval, maxMove int) []Keeper {
+	return []Keeper{&mouseMoveKeeper{interval: interval}}
+}
---
+++ keeper_windows_test.go
@@ -0,0 +1,81 @@
+//go:build windows
+
+package main
+
+import (
+	"testing"
+	"time"
+	"unsafe"
+
+	"golang.org/x/sys/windows"
+)
+
+type MockProc struct {
+	CallFunc func(...uintptr) (uintptr, uintptr, error)
+}
+
+func (m *MockProc) Call(a ...uintptr) (uintptr, uintptr, error) {
+	return m.CallFunc(a...)
+}
+
+type MockDLL struct {
+	Procs map[string]*MockProc
+}
+
+func (dll *MockDLL) NewProc(name string) *MockProc {
+	if proc, ok := dll.Procs[name]; ok {
+		return proc
+	}
+	return &MockProc{}
+}
+
+func TestMouseMoveKeeper_Name(t *testing.T) {
+	k := &mouseMoveKeeper{interval: 1}
+	if k.Name() != "mouse-move" {
+		t.Errorf("expected 'mouse-move', got '%s'", k.Name())
+	}
+}
+
+func TestMouseMoveKeeper_StartStop(t *testing.T) {
+	mockDLL := &MockDLL{
+		Procs: map[string]*MockProc{
+			"GetCursorPos": {
+				CallFunc: func(a ...uintptr) (uintptr, uintptr, error) {
+					pt := (*POINT)(unsafe.Pointer(a[0]))
+					pt.X, pt.Y = 100, 200
+					return 0, 0, nil
+				},
+			},
+			"SetCursorPos": {
+				CallFunc: func(a ...uintptr) (uintptr, uintptr, error) {
+					return 0, 0, nil
+				},
+			},
+		},
+	}
+
+	user32 = &windows.LazyDLL{}
+	procGetCursorPos = mockDLL.NewProc("GetCursorPos")
+	procSetCursorPos = mockDLL.NewProc("SetCursorPos")
+
+	k := &mouseMoveKeeper{interval: 1}
+	if err := k.Start(); err != nil {
+		t.Fatalf("Start failed: %v", err)
+	}
+
+	<-time.After(1500 * time.Millisecond)
+
+	if err := k.Stop(); err != nil {
+		t.Fatalf("Stop failed: %v", err)
+	}
+}
+
+func TestPlatformKeepers_Windows(t *testing.T) {
+	keepers := platformKeepers(180, 5)
+	if len(keepers) == 0 {
+		t.Fatal("expected at least one keeper for windows")
+	}
+	if keepers[0].Name() != "mouse-move" {
+		t.Errorf("expected first keeper to be 'mouse-move', got '%s'", keepers[0].Name())
+	}
+}
